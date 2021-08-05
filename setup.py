8c606c74b0958be75bbec1978981e78e294c4ba7
Nicholas Mancuso <nick.mancuso@gmail.com>
2021-08-04 21:27:08 -0700

    updated license

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
 )
